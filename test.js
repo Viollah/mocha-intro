ce73df95cbe33be93a5001b553703755d3a8aea2
Viollah <Viollah0119@gmail.com>
2021-07-20 08:27:15 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,17 @@
+describe('this test' , function(){
+    it('should show you how to use MochaJS' , function(){
+        assert.equal(2,2);
+
+        assert.deepEqual([2,2],[2,2]);
+    });
+    it("This should greet Viollah with 'Hello, Viollah'",function(){
+          assert.equal("Hello, Viollah",greet("Viollah"))
+    });
+    it("isDayName(weekDay,yearMonth)",function(){
+        assert.equal(isDayName('Saturday'), true);
+        assert.equal(isDayName('Monday'), true);
+        assert.equal(isDayName('January'), false, 'January is not a day');
+
+    });
+
+});
